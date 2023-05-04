75b09138e0d80a73eb321e64b0580af617ae6685
yhuan416 <1290635392@qq.com>
2023-05-04 15:25:48 +0800

    feat: update

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ install_path="../install/cJSON"
 mkdir -p build && cd build
 cmake ../cJSON -DBUILD_SHARED_AND_STATIC_LIBS=On -DCMAKE_INSTALL_PREFIX=${install_path}
 make
-# make install
+make install
